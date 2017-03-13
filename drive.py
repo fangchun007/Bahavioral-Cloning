fc585e2e5eddfe726294631587505751980f9a89
fangchun007 <fangchun007@gmail.com>
2017-03-13 10:39:23 +0200

    Add files via upload

--- drive.py
+++ drive.py
@@ -3,6 +3,10 @@ import base64
 import json
 import cv2
 
+import os
+import shutil
+from datetime import datetime
+
 import numpy as np
 import socketio
 import eventlet
@@ -14,6 +18,8 @@ from flask import Flask, render_template
 from io import BytesIO
 
 from keras.models import model_from_json
+from keras.models import load_model
+
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array
 
 # Fix error with Keras and TensorFlow
@@ -26,35 +32,86 @@ app = Flask(__name__)
 model = None
 prev_image_array = None
 
-def crop_image(image):
+IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS = 66, 200, 3
+INPUT_SHAPE = (IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
+
+def crop(image):
     return image[60:140,:,:]
 
-def enhance_contrast(image):
-    image_yuv = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
-    image_yuv[:,:,0] = cv2.equalizeHist(image_yuv[:,:,0])
-    return cv2.cvtColor(image_yuv, cv2.COLOR_YUV2BGR)
+def resize(image):
+    return cv2.resize(image, (IMAGE_WIDTH, IMAGE_HEIGHT), cv2.INTER_AREA)
+
+def rgb2yuv(image):
+    """
+    Will be used in the NVIDIA model
+    """
+    return cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+
+def preprocess(image):
+    image = crop(image)
+    image = resize(image)
+    image = rgb2yuv(image)
+    return image
+
+class SimplePIController:
+    def __init__(self, Kp, Ki):
+        self.Kp = Kp
+        self.Ki = Ki
+        self.set_point = 0.
+        self.error = 0.
+        self.integral = 0.
+
+    def set_desired(self, desired):
+        self.set_point = desired
+
+    def update(self, measurement):
+        # proportional error
+        self.error = self.set_point - measurement
+
+        # integral error
+        self.integral += self.error
+
+        return self.Kp * self.error + self.Ki * self.integral
+
+
+controller = SimplePIController(0.1, 0.002)
+set_speed = 15
+controller.set_desired(set_speed)
+
 
 @sio.on('telemetry')
 def telemetry(sid, data):
-    # The current steering angle of the car
-    steering_angle = data["steering_angle"]
-    # The current throttle of the car
-    throttle = data["throttle"]
-    # The current speed of the car
-    speed = data["speed"]
-    # The current image from the center camera of the car
-    imgString = data["image"]
-    image = Image.open(BytesIO(base64.b64decode(imgString)))
-    image_array = np.asarray(image)
-    image_array = crop_image(image_array)
-    image_array = enhance_contrast(image_array)
-    transformed_image_array = image_array[None, :, :, :]
-    # This model currently assumes that the features of the model are just the images. Feel free to change this.
-    steering_angle = float(model.predict(transformed_image_array, batch_size=1))
-    # The driving model currently just outputs a constant throttle. Feel free to edit this.
-    throttle = 0.2 # change to 0.5 for track-2
-    print(steering_angle, throttle)
-    send_control(steering_angle, throttle)
+    if data:
+        # The current steering angle of the car
+        steering_angle = data["steering_angle"]
+        # The current throttle of the car
+        throttle = data["throttle"]
+        # The current speed of the car
+        speed = data["speed"]
+        # The current image from the center camera of the car
+        imgString = data["image"]
+        image = Image.open(BytesIO(base64.b64decode(imgString)))
+        image_array = np.asarray(image)
+        image_array = preprocess(image_array)
+        transformed_image_array = image_array[None, :, :, :]
+        # This model currently assumes that the features of the model are just the images. Feel free to change this.
+        steering_angle = float(model.predict(transformed_image_array, batch_size=1))
+
+        # The driving model currently just outputs a constant throttle. Feel free to edit this.
+        throttle = controller.update(float(speed))
+    
+        print(steering_angle, throttle)
+        send_control(steering_angle, throttle)
+
+        # save frame
+        if args.image_folder != '':
+            timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
+            image_filename = os.path.join(args.image_folder, timestamp)
+            image.save('{}.jpg'.format(image_filename))
+    else:
+        # NOTE: DON'T EDIT THIS.
+        sio.emit('manual', data={}, skip_sid=True)
+
 
 
 @sio.on('connect')
@@ -64,30 +121,49 @@ def connect(sid, environ):
 
 
 def send_control(steering_angle, throttle):
-    sio.emit("steer", data={
-    'steering_angle': steering_angle.__str__(),
-    'throttle': throttle.__str__()
-    }, skip_sid=True)
+    sio.emit(
+        "steer",
+        data={
+            'steering_angle': steering_angle.__str__(),
+            'throttle': throttle.__str__()
+        },
+        skip_sid=True)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Remote Driving')
-    parser.add_argument('model', type=str,
-    help='Path to model definition json. Model weights should be on the same path.')
+    parser.add_argument(
+        'model',
+        type=str,
+        help='Path to model definition json. Model weights should be on the same path.'
+        )
+    parser.add_argument(
+        'image_folder',
+        type=str,
+        nargs='?',
+        default='',
+        help='Path to image folder. This is where the images from the run will be saved.'
+        )
+    
     args = parser.parse_args()
-    with open(args.model, 'r', encoding="utf-8") as jfile:
-        # NOTE: if saved the file by calling json.dump(model.to_json(), ...)
-        # then call:
-        #
-        #   model = model_from_json(json.loads(jfile.read()))
-        #
-        # instead.
-        model = model_from_json(jfile.read())
+    #with open(args.model, 'r', encoding="utf-8") as jfile:
+    #    model = model_from_json(jfile.read())
 
+    #model.compile("adam", "mse")
+    #weights_file = args.model.replace('json', 'h5')
+    #model.load_weights(weights_file)
+    model = load_model(args.model)
 
-    model.compile("adam", "mse")
-    weights_file = args.model.replace('json', 'h5')
-    model.load_weights(weights_file)
+    if args.image_folder != '':
+        print("Creating image folder at {}".format(args.image_folder))
+        if not os.path.exists(args.image_folder):
+            os.makedirs(args.image_folder)
+        else:
+            shutil.rmtree(args.image_folder)
+            os.makedirs(args.image_folder)
+        print("RECORDING THIS RUN ...")
+    else:
+        print("NOT RECORDING THIS RUN ...")
 
     # wrap Flask application with engineio's middleware
     app = socketio.Middleware(sio, app)
